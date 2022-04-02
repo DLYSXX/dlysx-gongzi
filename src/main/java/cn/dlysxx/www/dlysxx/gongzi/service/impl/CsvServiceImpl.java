f3941d8ba2810c7473ff37cc55e25cba0e36be63
Zhou Shuai <shuai.zhou@ipanda.co.jp>
2022-04-02 17:26:02 +0800

    Fix api use openapi generater

--- src/main/java/cn/dlysxx/www/dlysxx/gongzi/service/impl/CsvServiceImpl.java
+++ src/main/java/cn/dlysxx/www/dlysxx/gongzi/service/impl/CsvServiceImpl.java
@@ -1,12 +1,11 @@
 package cn.dlysxx.www.dlysxx.gongzi.service.impl;
 
 import cn.dlysxx.www.common.date.DateUtil;
-import cn.dlysxx.www.common.file.FileUtil;
 import cn.dlysxx.www.common.string.StringUtil;
-import cn.dlysxx.www.dlysxx.gongzi.service.CsvService;
+import cn.dlysxx.www.dlysxx.gongzi.api.CsvApiDelegate;
+import cn.dlysxx.www.dlysxx.gongzi.model.CsvApiCommand;
 import com.spire.xls.Workbook;
 import com.spire.xls.Worksheet;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -14,16 +13,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
-import java.util.Objects;
-import org.jooq.tools.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
 
 /**
  * Csv Service.
@@ -31,10 +29,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author shuai
  */
 @Service
-public class CsvServiceImpl implements CsvService {
-
-    @Value("${file.output.path}")
-    private String fileOutputPath;
+public class CsvServiceImpl implements CsvApiDelegate {
 
     @Value("${spring.mail.username}")
     private String from;
@@ -58,16 +53,15 @@ public class CsvServiceImpl implements CsvService {
     }
 
     @Override
-    public void readExcelData(MultipartFile multipartFile) {
-        try (InputStream inputStream =
-                 new FileInputStream(Objects.requireNonNull(multipartFileToFile(multipartFile)))) {
+    public ResponseEntity<Void> postV1CsvGongzi(CsvApiCommand csvApiCommand) {
+        try (InputStream inputStream = new FileInputStream(csvApiCommand.getCsvFilePath())) {
             Workbook workbook = new Workbook();
             workbook.loadFromStream(inputStream);
             Worksheet sheet = workbook.getWorksheets().get(0);
             int lastColumn = sheet.getLastColumn();
 
             final String ym = DateUtil.toString(LocalDateTime.now(), DateUtil.UUUUMM);
-            String backupPath = fileOutputPath + ym;
+            String backupPath = csvApiCommand.getOutputPath() + ym;
             if (!Files.isDirectory(Paths.get(backupPath))) {
                 Files.createDirectories(Paths.get(backupPath));
             }
@@ -76,9 +70,9 @@ public class CsvServiceImpl implements CsvService {
                 // get current record's user info
                 final String emailAddress = sheet.getRange().get(EMAIL_INDEX + i).getValue();
                 final String userName = sheet.getRange().get(USERNAME_INDEX + i).getValue();
-                final String emailTemplate = fileOutputPath + userName + HTML_MAIL_TEMPLATE;
+                final String emailTemplate = csvApiCommand.getOutputPath() + userName + HTML_MAIL_TEMPLATE;
                 final String emailTemplateBackup = backupPath + "/" + userName + HTML_MAIL_TEMPLATE;
-                if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(emailAddress)) {
+                if (StringUtils.hasLength(userName) || StringUtils.hasLength(emailAddress)) {
                     continue;
                 }
                 LOGGER.info("Current user info -----> username is {}, email is {}", userName, emailAddress);
@@ -95,29 +89,42 @@ public class CsvServiceImpl implements CsvService {
                 gongziCsvSheet.saveToHtml(emailTemplate);
 
                 // send mail
-                try {
-                    this.sendMail(emailAddress, emailTemplate);
-                    // backup email template
-                    Files.move(Paths.get(emailTemplate), Paths.get(emailTemplateBackup),
-                        StandardCopyOption.REPLACE_EXISTING);
-                    Thread.sleep(500);
-                } catch (Exception ex) {
-                    LOGGER.error("Send email failed!!! username is {}, email is {}", userName, emailAddress, ex);
-                }
+                this.sendMail(userName, emailAddress, emailTemplate, emailTemplateBackup);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     /**
-     * Send email by mailAddress and template path.
+     * Send Mail.
+     *
+     * @param userName user name
+     * @param emailAddress email address
+     * @param emailTemplate email template
+     * @param emailTemplateBackup email template backup
+     */
+    private void sendMail(String userName, String emailAddress, String emailTemplate, String emailTemplateBackup) {
+        try {
+            this.sender(emailAddress, emailTemplate);
+            // backup email template
+            Files.move(Paths.get(emailTemplate), Paths.get(emailTemplateBackup),
+                StandardCopyOption.REPLACE_EXISTING);
+            Thread.sleep(500);
+        } catch (Exception ex) {
+            LOGGER.error("Send email failed!!! username is {}, email is {}", userName, emailAddress, ex);
+        }
+    }
+
+    /**
+     * Mail sender.
      *
      * @param email email address
      * @param filePath mail template path
      * @throws Exception exception
      */
-    public void sendMail(String email, String filePath) throws Exception {
+    private void sender(String email, String filePath) throws Exception {
         javaMailSender.send(mimeMessage -> {
             try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
                 MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, StandardCharsets.UTF_8.name());
@@ -128,23 +135,5 @@ public class CsvServiceImpl implements CsvService {
             }
         });
         LOGGER.info("Send email succeed -----> email is {}", email);
-    }
-
-    /**
-     * Multipart file convert to file.
-     *
-     * @param file multipartFile
-     * @return file
-     * @throws Exception exception
-     */
-    private File multipartFileToFile(MultipartFile file) throws Exception {
-        if (ObjectUtils.isEmpty(file) || file.getSize() <= 0) {
-            return null;
-        }
-        InputStream ins = file.getInputStream();
-        File toFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        FileUtil.inputStreamToFile(ins, toFile);
-        ins.close();
-        return toFile;
     }
 }
